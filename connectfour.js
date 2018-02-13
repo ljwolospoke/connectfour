50c0ce4abdaea4d04eb5b44e02f83f25c9d89821
Lance Woloszyn <ljwolos@pointpark.edu>
2018-02-13 18:07:41 -0500

    still sorry

--- connectfour.js
+++ connectfour.js
@@ -1,11 +1,12 @@
-var connectfour = (function(){
+
+var connectfour = (function() {
 
   var grid = [[null, null, null, null, null, null, null],
               [null, null, null, null, null, null, null],
               [null, null, null, null, null, null, null],
               [null, null, null, null, null, null, null],
               [null, null, null, null, null, null, null],
-              [null, null, null, null, null, null, null]]
+              [null, null, null, null, null, null, null]];
 
               var current = "red";
 
@@ -15,7 +16,7 @@ var connectfour = (function(){
                 // RxCy
 
                 var x = Number(cell[1]);
-                var y = Number(cell[2])
+                var y = Number(cell[2]);
                  // this is the y in RxCy
                 // check if the cell is empty
                 if (grid[x][y] === null) {
@@ -33,6 +34,41 @@ var connectfour = (function(){
                   notify();
                 }
               }
+              function clearGrid() {
+                // set all cells to null
+                for (var r = 0; r < grid.length; r++) {
+                  for (var c = 0; c < grid[r].length; c++) {
+                    grid[r][c] = null;
+                  }
+                }
+                // notify all the listeners (which update the views)
+                notify();
+              }
+
+              // store all the listeners
+              // we have to call all of them if something changes
+              var listeners = [];
+
+              // this function registers/adds a listener
+              function listen(cb) {
+                // collect them in the listeners array
+                listeners.push(cb);
+              }
+
+              // notify all listeners of a change to the grid
+              function notify() {
+                // iterate through the array and call the listen callback function
+                for (var i = 0; i < listeners.length; i++) {
+                  // call the function
+                  listeners[i](grid);
+                }
+              }
+              return {
+                makeMove: makeMove,
+                clearGrid: clearGrid,
+                listen: listen
+              };
 
 
-});
+
+})();
